37a822350ae072caf4627cf84f63a2d786274a9b
Raoul Hess <raoul@kollarna.se>
2023-12-11 21:34:09 +0100

    Updating from workbook

--- nvim_current/init.lua
+++ nvim_current/init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.opt.spell = true
-vim.opt.spelllang = { "en_us", "sv_se" }
+vim.opt.spelllang = { "en_us", "sv" }
 vim.opt.langmenu = "en_us"
+vim.opt.tabstop = 4
+vim.opt.ts = 4
