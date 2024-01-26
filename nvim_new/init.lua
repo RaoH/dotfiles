4ceaa58b9651b95370b64dd3d45c4c449c2e9787
Raoul Hess <raoul@kollarna.se>
2024-01-26 16:49:06 +0100

    More settings and signwidth setup

--- nvim_new/init.lua
+++ nvim_new/init.lua
@@ -1,27 +1,20 @@
 -- LazyVim packet manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
-
-
 
 -- Vim options
 require("vim-options")
 
 -- Loading lazy
 require("lazy").setup("plugins")
-
-
-
-
-
--- nvim_new/lua/vim-options.lua
+++ nvim_new/lua/vim-options.lua
@@ -8,5 +8,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set relativenumber")
+vim.cmd("set nu rnu")
+vim.cmd("set signcolumn=yes:1")
 -- vim.cmd("set numberwidth=2")
 vim.g.mapleader = " "
