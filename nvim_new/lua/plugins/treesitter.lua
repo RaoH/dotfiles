99eb05f679c2ea41f9fd7e23c66960789db7f2e6
RaoH <raoul@blackbyte.se>
2024-02-06 20:54:07 +0100

    Added treesitter-context

--- nvim_new/lua/plugins/treesitter.lua
+++ nvim_new/lua/plugins/treesitter.lua
@@ -1,21 +1,27 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+			"nvim-telescope/telescope-file-browser.nvim",
 		},
-		"nvim-telescope/telescope-file-browser.nvim",
-	},
-	build = ":TSUpdate",
-	config = function()
-		local treesitter_conf = require("nvim-treesitter.configs")
+		build = ":TSUpdate",
+		config = function()
+			local treesitter_conf = require("nvim-treesitter.configs")
 
-		treesitter_conf.setup({
-			ensure_installed = { "lua", "elixir", "javascript", "html" },
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = true,
-		})
-	end,
+			treesitter_conf.setup({
+				ensure_installed = { "lua", "elixir", "javascript", "html" },
+				highlight = { enable = true },
+				indent = { enable = true },
+				autotag = true,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = { mode = "cursor", max_lines = 3 },
+	},
 }
