c02b701789d99ebcb68e7cd308f4258841a1bbbf
Raoul Hess <raoul@kollarna.se>
2024-03-22 13:22:23 +0100

    Moved to conform instead on none-ls

---
+++ nvim_new/lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- Conform will run multiple formatters sequentially
+					python = { "isort", "black" },
+					-- Use a sub-list to run only the first available formatter
+					javascript = { { "prettier", "prettierd", "eslint_d" } },
+					typescript = { "prettierd" },
+					javascriptreact = { "prettierd" },
+					typescriptreact = { "prettierd" },
+				},
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
+			})
+		end,
+	},
+}
