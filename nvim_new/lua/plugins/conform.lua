f83f3a629e9447139ca6cd3c2ac2354fb658fbed
RaoH <raoul@blackbyte.se>
2024-04-17 20:47:29 +0200

    Comform fixes

--- nvim_new/lua/plugins/conform.lua
+++ nvim_new/lua/plugins/conform.lua
@@ -6,20 +6,23 @@ return {
 			local conform = require("conform")
 			conform.setup({
 				formatters_by_ft = {
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					svelte = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					graphql = { "prettier" },
 					lua = { "stylua" },
-					-- Conform will run multiple formatters sequentially
-					python = { "isort", "black" },
-					-- Use a sub-list to run only the first available formatter
-					javascript = { { "prettier", "prettierd", "eslint_d" } },
-					typescript = { "prettierd" },
-					javascriptreact = { "prettierd" },
-					typescriptreact = { "prettierd" },
-					json = { "prettierd" },
 				},
 				format_on_save = {
-					-- These options will be passed to conform.format()
-					timeout_ms = 500,
 					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
 				},
 			})
 
