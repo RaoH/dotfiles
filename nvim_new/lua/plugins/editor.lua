7a71f29e47d554ff7c995f14c5555f6e75d14da9
RaoH <raoul@blackbyte.se>
2025-01-01 21:23:52 +0100

    Removed commented plugin added mini.ai

--- nvim_new/lua/plugins/editor.lua
+++ nvim_new/lua/plugins/editor.lua
@@ -1,4 +1,5 @@
 return {
+
 	{
 		"windwp/nvim-autopairs",
 		event = { "InsertEnter" },
@@ -21,49 +22,19 @@ return {
 		end,
 	},
 	{
+		"echasnovski/mini.ai",
+		version = false,
+		config = function()
+			require("mini.ai").setup({})
+		end,
+	},
+	{
 		"echasnovski/mini.surround",
 		version = false,
 		config = function()
 			require("mini.surround").setup({})
 		end,
 	},
-	-- {
-	-- 	"lukas-reineke/indent-blankline.nvim",
-	-- 	enabled = false,
-	-- 	config = function()
-	-- 		local highlight = {
-	-- 			"RainbowRed",
-	-- 			"RainbowYellow",
-	-- 			"RainbowBlue",
-	-- 			"RainbowOrange",
-	-- 			"RainbowGreen",
-	-- 			"RainbowViolet",
-	-- 			"RainbowCyan",
-	-- 		}
-	--
-	-- 		local hooks = require("ibl.hooks")
-	-- 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-	-- 			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-	-- 			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-	-- 			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-	-- 			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-	-- 			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-	-- 			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-	-- 			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-	-- 		end)
-	--
-	-- 		require("ibl").setup({
-	-- 			exclude = { filetypes = { "dashboard" } },
-	-- 			indent = { highlight = highlight },
-	-- 			scope = {
-	-- 				show_start = false,
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- 	main = "ibl",
-	-- 	opts = {},
-	-- },
-	--TODO Might be possible to switch this up.
 	{
 		"windwp/nvim-ts-autotag",
 		config = function()
