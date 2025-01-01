25c3d22e0c9a4de44e39ab17e35b17a9ea6ebe53
RaoH <raoul@blackbyte.se>
2025-01-01 21:23:17 +0100

    Removed and remapped window handling

--- nvim_new/lua/config/keymaps.lua
+++ nvim_new/lua/config/keymaps.lua
@@ -6,28 +6,15 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
-
--- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
-
 --Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+keymap.set("n", "ms", ":split<Return>", opts)
+keymap.set("n", "mv", ":vsplit<Return>", opts)
 
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
-
--- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+keymap.set("n", "mh", "<C-w>h")
+keymap.set("n", "mk", "<C-w>k")
+keymap.set("n", "mj", "<C-w>j")
+keymap.set("n", "ml", "<C-w>l")
 
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
