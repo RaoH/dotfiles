d2e5356274f1e9895224812df5dcfeb895650c39
RaoH <raoul@blackbyte.se>
2025-01-01 21:24:20 +0100

    Added highlights for indent

--- nvim_new/lua/plugins/snacks.lua
+++ nvim_new/lua/plugins/snacks.lua
@@ -1,4 +1,13 @@
 local dashboard_settings = require("config.dashboard")
+
+vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
@@ -6,20 +15,34 @@ return {
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+		-- refer to the configuratikkon section below
 		git = { enabled = true },
 		dim = { enabled = true },
 		bigfile = { enabled = true },
 		bufdelete = { enabled = true },
 		dashboard = { preset = { header = dashboard_settings.logo }, sections = dashboard_settings.dashboard_layout_section },
-		indent = { animate = { enabled = false } },
+		indent = {
+			animate = { enabled = false },
+			indent = {
+				hl = {
+					"RainbowRed",
+					"RainbowYellow",
+					"RainbowBlue",
+					"RainbowOrange",
+					"RainbowGreen",
+					"RainbowViolet",
+					"RainbowCyan",
+				}
+			}
+		},
 		notifier = { enabled = true, timeout = 3000 },
-		quickfile = { enabled = true },
+		quickfiles = { enabled = true },
 		scroll = { enabled = true },
-		statuscolumn = { enabled = false },
+		statuscolumn = { enabled = true },
 		words = { enabled = true },
 		zen = { enabled = true },
 		lazygit = { enabled = true },
 		toggle = { enabled = true },
+		terminal = { enabled = true }
 	},
 }
