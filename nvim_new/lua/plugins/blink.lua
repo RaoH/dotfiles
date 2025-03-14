f8dabb0b0160bd143a3dd7f29d658b489c80e0a5
RaoH <raoul@blackbyte.se>
2025-03-14 21:49:25 +0100

    Finally got snippets to work again

--- nvim_new/lua/plugins/blink.lua
+++ nvim_new/lua/plugins/blink.lua
@@ -2,132 +2,95 @@ local trigger_text = ";"
 
 return {
 	{
-		"xzbdmw/colorful-menu.nvim",
-		config = function()
-			require("colorful-menu").setup({
-				ls = {
-					lua_ls = {
-						-- Maybe you want to dim arguments a bit.
-						arguments_hl = "@comment",
-					},
-					gopls = {
-						-- When true, label for field and variable will format like "foo: Foo"
-						-- instead of go's original syntax "foo Foo".
-						add_colon_before_type = false,
-					},
-					-- for lsp_config or typescript-tools
-					ts_ls = {
-						extra_info_hl = "@comment",
-					},
-					vtsls = {
-						extra_info_hl = "@comment",
-					},
-					["rust-analyzer"] = {
-						-- Such as (as Iterator), (use std::io).
-						extra_info_hl = "@comment",
-					},
-					clangd = {
-						-- Such as "From <stdio.h>".
-						extra_info_hl = "@comment",
-					},
-					roslyn = {
-						extra_info_hl = "@comment",
-					},
-					basedpyright = {
-						-- It is usually import path such as "os"
-						extra_info_hl = "@comment",
-					},
+		'saghen/blink.cmp',
+		dependencies = { 'rafamadriz/friendly-snippets', { 'L3MON4D3/LuaSnip', version = 'v2.*' } },
 
-					-- If true, try to highlight "not supported" languages.
-					fallback = true,
+		-- use a release tag to download pre-built binaries
+		version = '*',
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = { preset = "enter" },
+			completion = {
+				menu = {
+					border = "rounded",
 				},
-				-- If the built-in logic fails to find a suitable highlight group,
-				-- this highlight is applied to the label.
-				fallback_highlight = "@variable",
-				-- If provided, the plugin truncates the final displayed text to
-				-- this width (measured in display cells). Any highlights that extend
-				-- beyond the truncation point are ignored. Default 60.
-				max_width = 60,
-			})
-		end,
-	},
-
-	{
-		"saghen/blink.cmp",
-		lazy = false,
-		dependencies = "rafamadriz/friendly-snippets",
-		version = "*",
-		config = function()
-			require("blink-cmp").setup({
-				keymap = { preset = "enter" },
-				completion = {
-					menu = {
+				documentation = {
+					window = {
 						border = "rounded",
-						draw = {
-							columns = { { "kind_icon" }, { "label", gap = 1 } },
-							components = {
-								label = {
-									width = { fill = true, max = 60 },
-									text = require("colorful-menu").blink_components_text,
-									highlight = require("colorful-menu").blink_components_highlight,
+					},
+					auto_show = true,
+					auto_show_delay_ms = 300,
+				},
+				trigger = {
+					show_on_keyword = true,
+				}
+
+				-- ghost_text = { enabled = true }
+			},
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			cmdline = { enabled = false },
+			sources = {
+				--default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+				default = { 'lazydev', 'lsp', 'snippets', 'buffer' },
+				transform_items = function(ctx, items)
+					local _ = ctx
+					local col = vim.api.nvim_win_get_cursor(0)[2]
+					local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+					local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+					if trigger_pos then
+						for _, item in ipairs(items) do
+							item.textEdit = {
+								newText = item.insertText or item.label,
+								range = {
+									start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
+									["end"] = { line = vim.fn.line(".") - 1, character = col },
 								},
-							},
-						},
-					},
-					documentation = {
-						window = {
-							border = "rounded",
-						},
-						auto_show = true,
-						auto_show_delay_ms = 300,
-					},
-					-- ghost_text = { enabled = true }
-				},
-
-				appearance = {
-					use_nvim_cmp_as_default = true,
-					nerd_font_variant = "mono",
-				},
-				sources = {
-
-					default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-					cmdline = {},
-					transform_items = function(ctx, items)
-						local _ = ctx
-						local col = vim.api.nvim_win_get_cursor(0)[2]
-						local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-						local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
-						if trigger_pos then
-							for _, item in ipairs(items) do
-								item.textEdit = {
-									newText = item.insertText or item.label,
-									range = {
-										start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
-										["end"] = { line = vim.fn.line(".") - 1, character = col },
-									},
-								}
-							end
-							return vim.tbl_filter(function(item)
-								return item.kind == require('blink.cmp.types').CompletionItemKind.Snippet
-							end, items)
-						else
-							return vim.tbl_filter(function(item)
-								return item.kind ~= require('blink.cmp.types').CompletionItemKind.Snippet
-							end, items)
+							}
 						end
-					end,
-					providers = {
-						lazydev = {
-							name = "LazyDev",
-							module = "lazydev.integrations.blink",
-							-- make lazydev completions top priority (see `:h blink.cmp`)
-							score_offset = 100,
-						},
+						return vim.tbl_filter(function(item)
+							vim.notify_once("Testing")
+							return item.kind == require('blink.cmp.types').CompletionItemKind.Snippet
+						end, items)
+					else
+						return vim.tbl_filter(function(item)
+							return item.kind ~= require('blink.cmp.types').CompletionItemKind.Snippet
+						end, items)
+					end
+				end,
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
 					},
+					-- snippets = {
+					-- 	should_show_items = function(ctx)
+					-- 		vim.notify('triggered')
+					-- 		return ctx.trigger.initial_kind ~= ';'
+					-- 	end
+					-- }
 				},
-				snippets = { preset = 'luasnip' },
-				signature = { enabled = true }
-			})
-		end,
+			},
+			snippets = { preset = 'luasnip' },
+			signature = { enabled = true }
+		},
 	},
+	-- 	{
+	-- 		"xzbdmw/colorful-menu.nvim",
+	-- 		config = function()
+	-- 			require("colorful-menu").setup()
+	-- 		end,
+	-- 	},
+	-- }
+
 }
